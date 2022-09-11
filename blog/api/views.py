f731b01db7f6a47bf34b6459c383593aecc63ff1
xtmat <atulkumar281725379@gmail.com>
2022-09-12 00:07:54 +0530

    finished authentication

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication
 
 class PostList(generics.ListCreateAPIView):
+    authentication_classes = [SessionAuthentication,TokenAuthentication]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
